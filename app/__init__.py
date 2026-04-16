d884dfaa74e57f59fb08a9508ce5dae96db9a1d0
Gloria Azum <azumgloria@gmail.com>
2026-04-17 00:33:20 +0200

    updating read me

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 from flask_migrate import Migrate
+from . import models
+from .routes import main
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -17,7 +19,9 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes import main
     app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
 
     return app
